f43cee732ba91c6307823d0154c95236760734e6
Daewook123 <cdo5080@gmail.com>
2025-01-13 09:42:45 +0900

    Dto

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,16 +1,36 @@
+type PostDto = {
+  id: number;
+  createDate: string;
+  modifyDate: string;
+  authorId: number;
+  authorName: string;
+  title: string;
+  published: boolean;
+  listed: boolean;
+};
+type PostItemPageDto = {
+  currentPageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+  items: PostDto[];
+};
+
 export default async function Page() {
   const response = await fetch("http://localhost:8080/api/v1/posts");
-  const body = await response.json();
+  const body: PostItemPageDto = await response.json();
 
   return (
     <div>
-      <div>currentPageNumber:{body.currentPageNumber}</div>
-      <div>pageSize:{body.pageSize}</div>
-      <div>totalPage:{body.totalPage}</div>
-      <div>totalItems:{body.totalItems}</div>
+      <div>
+        <div>currentPageNumber: {body.currentPageNumber}</div>
+        <div>pageSize: {body.pageSize}</div>
+        <div>totalPages: {body.totalPages}</div>
+        <div>totalItems: {body.totalItems}</div>
+      </div>
       <hr />
       <ul>
-        {body.items.map((item: any) => (
+        {body.items.map((item: PostDto) => (
           <li key={item.id} className="border-[2px] border-[red] my-3">
             <div>id : {item.id}</div>
             <div>createDate : {item.createDate}</div>
@@ -18,8 +38,8 @@ export default async function Page() {
             <div>authorId : {item.authorId}</div>
             <div>authorName : {item.authorName}</div>
             <div>title : {item.title}</div>
-            <div>published : {item.published}</div>
-            <div>listed : {item.listed}</div>
+            <div>published : {`${item.published}`}</div>
+            <div>listed : {`${item.listed}`}</div>
           </li>
         ))}
       </ul>
